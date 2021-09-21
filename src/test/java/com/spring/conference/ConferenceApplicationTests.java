070f6f4eb36f3d9c9680a19bf4207ebebe9625c0
rubingiurgiu <43540577+rubingiurgiu@users.noreply.github.com>
2021-09-21 14:50:19 +0300

    intial configuration of project

---
+++ src/test/java/com/spring/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
